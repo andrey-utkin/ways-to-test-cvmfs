e5133596990baa63e4b0aac610d644a0b5bfe227
Andriy Utkin <hello@autkin.net>
2026-06-15 18:30:48 +0000

    Add Archlinux quirks

--- test.runner.with-systemd.bash
+++ test.runner.with-systemd.bash
@@ -9,6 +9,17 @@ export USER=$CVMFS_TEST_USER # shouldn't be needed, but...
 #export CVMFS_SERVER_DEBUG=4 # 4=rr, see cvmfs_server_coda.sh
 export CVMFS_SERVER_DEBUG=3 # 3 means debug binary, see cvmfs_server_coda.sh
 pushd test
+
+# Necessary only for Archlinux, actually
+if [[ -d /etc/httpd/conf/conf.d ]]; then
+	sudo -E cvmfs_config setup
+	sudo ln -snvf conf/conf.d /etc/httpd/conf.d
+	sudo sed -i /etc/httpd/conf/httpd.conf -e '/LoadModule proxy_module modules\/mod_proxy.so/s/^#//g'
+	sudo sed -i /etc/httpd/conf/httpd.conf -e '/LoadModule expires_module modules\/mod_expires.so/s/^#//g'
+	sudo systemctl restart httpd || true # what if it's called differently...
+fi
+
+
 set +e
 #bash -x ./run.sh /dev/stdout -d src/"$1"
 #bash -x ./run.sh /var/log/test.log -d src/"$1" # /var/log/test.log: permission denied
